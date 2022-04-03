62fbc49be9c623ad4c42266fcd9726a450aec595
Nikolay <nikolyand@gmail.com>
2022-04-04 00:26:29 +0300

    add jpa-entities module

---
+++ jpa-entities/src/main/java/com/prokopchuk/modules/User.java
@@ -0,0 +1,41 @@
+package com.prokopchuk.modules;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.Hibernate;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+public class User {
+    @Id
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        User user = (User) o;
+        return id != null && Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
