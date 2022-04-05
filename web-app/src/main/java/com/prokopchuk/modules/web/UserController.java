bbcaa9d3098af6207caa84d559b8d91fd5183846
Nikolay <nikolyand@gmail.com>
2022-04-05 10:06:35 +0300

    add web-app module

---
+++ web-app/src/main/java/com/prokopchuk/modules/web/UserController.java
@@ -0,0 +1,11 @@
+package com.prokopchuk.modules.web;
+
+import com.prokopchuk.modules.User;
+import com.prokopchuk.modules.domain.UserCommand;
+
+public class UserController {
+
+    User saveUser(UserCommand command) {
+        return new User();
+    }
+}
